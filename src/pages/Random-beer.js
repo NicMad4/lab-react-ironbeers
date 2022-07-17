54f9b5569a04b1ba2d522fa833a603f699ed1c69
Nico M <nico_16bcn@hotmail.com>
2022-07-17 23:37:58 +0200

    iteracion 5

--- src/pages/Random-beer.js
+++ src/pages/Random-beer.js
@@ -1,13 +1,43 @@
-import {Link} from "react-router-dom"
+
+import {Link,useParams} from "react-router-dom"
 import gohome from "../assets/gohome.jpg";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import React from "react"
 
 function RandomBeer() {
+  const [randomBeer, setRandomBeer] = useState([]);
+  const params = useParams();
+  console.log('hola',params);
+
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then((response) => {
+        
+        setRandomBeer(response.data);
+      })
+  }, []);
+
     return (
-     <div>
-      <Link to={`/`}><img src={gohome} alt="homepage"/></Link>
-      <p>Random Beer</p>
- 
-     </div>
+
+      <div>
+      <header>
+        <Link to={"/"}>
+          <img src={gohome} alt='gohome' />
+        </Link>
+      </header>
+        <div className="cervezaRandom">
+          <img id='onebeer' src={randomBeer.image_url} alt='gohome'></img>
+          <h2>{randomBeer.name}</h2>
+          <h3>{randomBeer.tagline}</h3>
+          <p className="brewed">First brewed: {randomBeer.first_brewed} </p>
+          <p className="attenuation">attenuation level: {randomBeer.attenuation_level}</p>
+          <p className="description">Description: {randomBeer.description} </p>
+          <p>By:{randomBeer.contributed_by}</p>
+        </div>
+      
+    </div>
    );
  }
  
